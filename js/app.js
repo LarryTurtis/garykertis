08cbd3d6765a15ad29ba5e9fb519087fed2d5d11
Gary Kertis <gary.kertis@gmail.com>
2014-04-11 17:02:36 -0400

    Finished refactoring

---
+++ js/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+var myApp = angular.module('myApp', [
+  'ngRoute',
+  'ngAnimate',
+  'myApp.filters',
+  'myApp.services',
+  'myApp.directives',
+  'myApp.controllers'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/sites/:siteId', {templateUrl: 'partials/siteDetail.html', controller: 'siteDetailCtrl'}).
+	otherwise({redirectTo: '/sites/vivo-para'});
+}]);
+
+
+
