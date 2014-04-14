39885286d57b5a7db7c9281a47d365b0b88a207e
Gary Kertis <gary.kertis@gmail.com>
2014-04-14 04:01:31 -0400

    lint and rewrite slants.js

--- js/controllers.js
+++ js/controllers.js
@@ -11,9 +11,9 @@ myAppControllers.controller('siteCtrl', ['$scope', '$http', '$location',
 
     $scope.getClass = function(path) {
      if ($location.path().substr(0, path.length) == path) {
-      return "active"
+      return 'active';
     } else {
-      return ""
+      return '';
     }
     };
 
@@ -25,7 +25,7 @@ myAppControllers.controller('siteDetailCtrl', ['$scope', '$routeParams', '$http'
   function($scope, $routeParams, $http) {
     $http.get('sites/' + $routeParams.siteId + '.json').success(function(data) {
       $scope.site = data;
-      fire();
+      animate(true);
     });
   
   }]);
