193e1ee65f17bfed3783dc441f43db0baa49c0d4
Gary Kertis <garykertis@PUBWS76.local>
2014-09-17 17:05:00 -0400

    Add Moby Dick

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ var myApp = angular.module('myApp', [
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/sites/:siteId', {templateUrl: 'partials/siteDetail.html', controller: 'siteDetailCtrl'}).
   when('/goals', {templateUrl: 'partials/goals.html', controller: 'siteDetailCtrl'}).
-	otherwise({redirectTo: '/sites/vivo-para'});
+	otherwise({redirectTo: '/sites/moby-dick'});
 }]);
 
 
